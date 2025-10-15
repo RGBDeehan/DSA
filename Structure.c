fc71d6a64274a56f5abe12ec91654fd409591825
Rageeb Yasar Deehan <rageebyasardeehan@gmail.com>
2025-10-15 18:55:49 +0600

    Add files via upload

---
+++ Structure.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct ABC {
+
+    uint16_t id;
+    float cgpa;
+
+};
+
+int main(){
+
+    // struct ABC student;
+
+    // printf("Enter student id\t:\t");
+    // scanf("%u",&student.id);
+
+    // printf("\nEnter student CGPA\t:\t");
+    // scanf("%f",&student.cgpa);
+
+    // printf("\nYour student id\t:\t%u", student.id);
+    // printf("\nYour CGPA\t:\t%.2f", student.cgpa);
+
+    // struct ABC student[3];
+
+    // for(int i=0; i<3; i++){
+    
+    // printf("\nEnter student id\t:\t");
+    // scanf("%u",&student[i].id);
+
+    // printf("\nEnter student CGPA\t:\t");
+    // scanf("%f",&student[i].cgpa);
+
+    // printf("\n");
+    // printf("---------------------------------------\n");
+
+    // }
+
+    // for(int i=0; i<3; i++){
+
+    // printf("\nYour student id\t:\t%u", student[i].id);
+    // printf("\nYour CGPA\t:\t%.2f", student[i].cgpa);
+
+    // printf("\n---------------------------------------");
+
+    // }
+
+
+    return 0;
+}
